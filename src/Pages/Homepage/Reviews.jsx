3d9abaece1122fc4f845ad613fc42be27741691f
Adegbuyi Daniel <adegbuyidaniel42@gmail.com>
2023-11-11 12:01:48 +0100

    Update

--- src/Pages/Homepage/Reviews.jsx
+++ src/Pages/Homepage/Reviews.jsx
@@ -1,54 +1,61 @@
+import PropTypes from 'prop-types';
+import './Reviews.css';
 import React from 'react';
+
 const ReviewTitle = () => {
     return (
-        <h1 style={{ width: '100%', color: '#121415', fontSize: 32, fontFamily: 'Aeonik', fontWeight: '700', wordWrap: 'break-word' }}>Our Reviews</h1>
+        <h1 className="review-title">Our Reviews</h1>
     );
 }
 
 const ReviewContent = () => {
     return (
-        <div style={{ width: '100%', textAlign: 'center', color: 'rgba(18, 20, 21, 0.40)', fontSize: 18, fontFamily: 'Aeonik', fontWeight: '500', wordWrap: 'break-word' }}>As a fish farmer, getting capital to produce at a very large scale to meet up with consumer demands have been a difficulty for me, but getting access to low interest loans through FarmFortify has proven a stress free way of transitioning into large-scale production.</div>
+        <div className="review-content">As a fish farmer, getting capital to produce at a very large scale to meet up with consumer demands have been a difficulty for me, but getting access to low interest loans through FarmFortify has proven a stress free way of transitioning into large-scale production.</div>
     );
 }
 
 const ReviewAuthor = ({ name }) => {
     return (
-        <div style={{ width: '100%', height: '100%', color: '#121415', fontSize: 18, fontFamily: 'Aeonik', fontWeight: '700', wordWrap: 'break-word' }}>{name}</div>
+        <h2 className="review-author">{name}</h2>
     );
 }
 
+ReviewAuthor.propTypes = {
+    name: PropTypes.string.isRequired
+};
+
 const ReviewOccupation = ({ occupation }) => {
     return (
-        <div style={{ width: '100%', height: '100%', color: '#33383D', fontSize: 16, fontFamily: 'Aeonik', fontWeight: '500', wordWrap: 'break-word' }}>{occupation}</div>
+        <h3 className="review-occupation">{occupation}</h3>
     );
 }
 
 const ReviewQuote = () => {
     return (
-        <div style={{ width: '100%', textAlign: 'center' }}>
-            <span style={{ color: '#121415', fontSize: 18, fontFamily: 'Aeonik', fontWeight: '500', wordWrap: 'break-word' }}>“FarmFortify has given me us access to quality input that has in turn improved the quality of our crops. Quality Fertilizers are now being delivered to our doorsteps and our <br />farm produce are sold with greater value, unlike in those days</span>
-            <span style={{ color: '#121415', fontSize: 14, fontFamily: 'Aeonik', fontWeight: '400', wordWrap: 'break-word' }}">.”</span>
+        <div className="review-quote">
+            <span className="quote-text">“FarmFortify has given me us access to quality input that has in turn improved the quality of our crops. Quality Fertilizers are now being delivered to our doorsteps and our <br />farm produce are sold with greater value, unlike in those days</span>
+            <span className="quote-author">.”</span>
         </div>
     );
 }
 
 const ReviewImage = ({ imageSrc }) => {
     return (
-        <img style={{ width: '100%', height: '100%', borderRadius: 9999 }} src={imageSrc} />
+        <img className="review-image" src={imageSrc} alt="Profile Picture" />
     );
 }
 
-const Reviews = () => {
+const Reviews = ({ content, author, occupation, quote, imageSrc }) => {
     return (
         <>
             <ReviewTitle />
-            <ReviewContent />
-            <ReviewAuthor name="Olusegun Sulaiman" />
-            <ReviewOccupation occupation="Crop Farmer in Oyo State" />
-            <ReviewQuote />
-            <ReviewImage imageSrc="https://example.com/ProfilePicture.png" />
-            <ReviewAuthor name="Olusegun Sulaiman" />
-            <ReviewOccupation occupation="Crop Farmer in Oyo State" />
+            <ReviewContent content={content} />
+            <ReviewAuthor name={author} />
+            <ReviewOccupation occupation={occupation} />
+            <ReviewQuote quote={quote} />
+            <ReviewImage imageSrc={imageSrc} />
+            <ReviewAuthor name={author} />
+            <ReviewOccupation occupation={occupation} />
         </>
     );
 }
