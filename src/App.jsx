e30ba9af0fe0820765465539278c9df43a53723c
latiniuxs <koredeoladetohun@gmail.com>
2023-11-11 19:19:33 +0100

    getting started

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
-import Navbar from "./Components/Navbar"
-import {Routes,Route} from 'react-router-dom'
-import Homepage from "./Pages/Homepage/Homepage"
+import Navbar from "./Components/Navbar";
+import { Routes, Route } from "react-router-dom";
+import Homepage from "./Pages/Homepage/Homepage";
+import About from "./Pages/aboutUs/About";
 
 function App() {
   return (
     <>
-      <Navbar/>
-      <Homepage/>
-      
+        <Navbar/>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
